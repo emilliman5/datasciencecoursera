bbb7a0de77b921a3987c9bd25efb4ba869c7d469
emilliman5 <emilliman5@users.noreply.github.com>
2014-08-11 19:57:11 -0400

    added comments to code

--- EDA/Project_2/plot6.R
+++ EDA/Project_2/plot6.R
@@ -1,15 +1,23 @@
-#Exploratory Data Analysis Course Project 2 plot5
-
+#Exploratory Data Analysis Course Project 2 plot6
+library(ggplot2)
+library(reshape2)
 #read in the data
 
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
+#find SCC Ids for motor vehicles (Mobile, vehicles, I exculded non-road)
 Ids<-SCC[grep("Vehicles", SCC$EI.Sector),1]
 
-#calcuate total emissions by year
-totals<-tapply(NEI[NEI$fips=="24510" & NEI$SCC %in% Ids,4], as.factor(NEI[NEI$fips=="24510" & NEI$SCC %in% Ids,6]), sum)
+#calcuate total emissions by year and locations
+totals<-tapply(NEI[(NEI$fips=="24510" | NEI$fips=="06037")& NEI$SCC %in% Ids,4], 
+               list(as.factor(NEI[(NEI$fips=="24510" | NEI$fips=="06037") & NEI$SCC %in% Ids,6]), 
+                    as.factor(NEI[(NEI$fips=="24510" | NEI$fips=="06037") & NEI$SCC %in% Ids,1])), 
+                sum)
 
-png("plot5.png", width=600, height=600)
-plot(c(1999,2002,2005,2008),totals, type="l", main="PM2.5 from Vehicles in Baltimore City", lwd=2, xlab="Year", ylab="Total Emissions")
+#transform the data to tall and skinny for easy factoring
+emissions<-melt(totals)
+
+png("plot6.png", width=800, height=800)
+qplot(X1, value, data=emissions, color= as.factor(X2), geom="line", main="Vehicle emissions: Baltimore vs. LA", aes(size=1), xlab="Year", ylab="Total Emissions")
 dev.off()
