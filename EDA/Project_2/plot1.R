51adffe00e300f0804e6948f4d3b6fd75d64d998
emilliman5 <emilliman5@users.noreply.github.com>
2014-08-24 08:31:18 -0400

    Added title to plot

--- EDA/Project_2/plot1.R
+++ EDA/Project_2/plot1.R
@@ -9,5 +9,5 @@ SCC <- readRDS("Source_Classification_Code.rds")
 totals<-tapply(NEI$Emissions, as.factor(NEI$year), sum)
 
 png("plot1.png", width=600, height=600)
-plot(c(1999,2002,2005,2008),totals, type="h", lwd=10, xlab="Year", ylab="Total Emissions")
+plot(c(1999,2002,2005,2008),totals, type="h", lwd=10, xlab="Year", ylab="Total Emissions", main = "Total PM2.5 Emissions")
 dev.off()
